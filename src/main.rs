7670916498124e405f2f47771a722b8efd178bec
nomissbowling <nomissbowling@yahoo.co.jp>
2024-03-15 10:12:31 +0900

    version 3.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/crosstermine/3.1.0")]
+#![doc(html_root_url = "https://docs.rs/crosstermine/3.2.0")]
 //! crosstermine mine for Rust with crossterm
 //!
 
@@ -16,7 +16,7 @@ use prayterm::{PrayTerm, Rgb, NopColor};
 
 use minefield::MineField;
 
-use mvc_rs::View as MVCView;
+use mvc_rs::{TPacket, TView};
 
 /// Term
 pub struct Term<T> {
@@ -26,11 +26,13 @@ pub struct Term<T> {
   pub tm: PrayTerm
 }
 
-/// trait MVCView for Term
-impl<T: NopColor + Clone> MVCView<T> for Term<T> {
+/// trait TView for Term
+impl<T: NopColor + Clone> TView<T> for Term<T> {
   /// wr
-  fn wr(&mut self, x: u16, y: u16, st: u16,
-    bgc: u16, fgc: u16, msg: &String) -> Result<(), Box<dyn Error>> {
+  fn wr(&mut self, p: impl TPacket) -> Result<(), Box<dyn Error>> {
+    let v = p.to_vec();
+    let (x, y, st, bgc, fgc) = (v[0], v[1], v[2], v[3], v[4]);
+    let msg = &p.as_str().to_string();
     self.tm.wr(x, y, st, self.col(bgc), self.col(fgc), msg)?;
     Ok(())
   }
